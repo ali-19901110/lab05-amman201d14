2b84b01ed842358efca2e75098d2009e068673cc
ali <alikasasbeh60@yahoo.com>
2021-02-11 14:48:36 +0200

    sumAndMultiply

--- app.js
+++ app.js
@@ -52,11 +52,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) {
+  let z =sum(a,b);
+  let sm =sum(z[0],c);
+  let y=multiply(a,b);
+  let mul=multiply(y[0],c);
   //eslint-disable-line
+  //console.log([sm,mul,`${a} and ${b} and ${c} sum to ${sm}.`,`The product of ${a} and ${b} and ${c} is ${mul}.`]);
+  return[sm[0],mul[0],`${a} and ${b} and ${c} sum to ${sm[0]}.`,`The product of ${a} and ${b} and ${c} is ${mul[0]}.`];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
